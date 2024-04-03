8ed145f80d0fbc278d94c928d5219eae8b29030d
Tanyal41 <165917318+Tanyal41@users.noreply.github.com>
2024-04-03 17:08:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript для меню-бутерброду
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
